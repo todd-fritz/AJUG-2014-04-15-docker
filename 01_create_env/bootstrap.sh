7cb9e2f9bd16afe73d6833e1aafaadfddfb7a13e
Todd Fritz <todd.fritz@inbloom.org>
2014-04-09 16:41:09 -0400

    Updated to Ubuntu Saucy.

--- 01_create_env/bootstrap.sh
+++ 01_create_env/bootstrap.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-apt-get update
+sudo apt-get update
+sudo apt-get install linux-image-extra-`uname -r`
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+
+sudo sh -c "echo deb http://get.docker.io/ubuntu docker main\
+> /etc/apt/sources.list.d/docker.list"
+
+sudo apt-get update
+
 sudo apt-get install -y curl
+
 curl -sL https://get.docker.io/ | sh
 
 sudo usermod -aG docker vagrant
-
 
 
