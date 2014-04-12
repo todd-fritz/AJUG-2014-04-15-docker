7df4f990a7128f5d121b9f408ba28b22985b52af
Todd Fritz <todd.fritz@inbloom.org>
2014-04-12 11:03:27 -0400

    Updated Ubuntu and bootstrap script.

--- 01_create_env/bootstrap.sh
+++ 01_create_env/bootstrap.sh
@@ -2,14 +2,14 @@
 
 sudo apt-get update
 sudo apt-get install linux-image-extra-`uname -r`
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 
 sudo sh -c "echo deb http://get.docker.io/ubuntu docker main\
 > /etc/apt/sources.list.d/docker.list"
 
 sudo apt-get update
 
-sudo apt-get install -y curl
+sudo apt-get install -y curl git
 
 curl -sL https://get.docker.io/ | sh
 
